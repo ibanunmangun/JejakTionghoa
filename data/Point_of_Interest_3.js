531b3e59c5611e44dc823b249601fdad0c2ad604
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-01-07 07:22:51 +0700

    Add files via upload

---
+++ data/Point_of_Interest_3.js
@@ -0,0 +1,27 @@
+var json_Point_of_Interest_3 = {
+"type": "FeatureCollection",
+"name": "Point_of_Interest_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Klenteng Tek Seng Bio", "X": 107.1425705, "Y": -6.2548874, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.142570500000033, -6.254887399999973 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Gedung Juang 45", "X": 107.0546198, "Y": -6.2592573, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.054619800000069, -6.259257299999945 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Pasar Proyek", "X": 107.0044428, "Y": -6.2402249, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.004442800000049, -6.2402249 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Klenteng Hok Lay Kiong", "X": 107.00317478, "Y": -6.2440567949799997, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.003193323472033, -6.244068053953779 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Rumah Makan 889", "X": 107.002862, "Y": -6.2442995, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.002862, -6.244299499999954 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Chinese Food 88", "X": 106.9917807, "Y": -6.2439529, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.991780700000049, -6.243952899999954 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Masjid Agung Nurut Taqwa", "X": 107.121121, "Y": -6.268803, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.121121, -6.268803 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "SPBU Cikarang", "X": 107.137866, "Y": -6.262419, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.137866, -6.262419 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Apotek dan Klinik Kimia Farma 507 Cibitung", "X": 107.104575, "Y": -6.273145, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.104575, -6.273145 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "RSUD Kabupaten Bekasi", "X": 107.083141, "Y": -6.266793, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.083141, -6.266793 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Kantor Pemadam Kebakaran Tambun Selatan", "X": 107.054642, "Y": -6.259761, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.054642, -6.259761 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Polsek Cikarang Barat", "X": 107.103691, "Y": -6.273223, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.103691, -6.273223 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "ATM Center Superindo Bulak Kapal", "X": 107.023498, "Y": -6.249527, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.023498, -6.249527 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Money Changer - Pepe Valas", "X": 107.003435, "Y": -6.241144, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.003435, -6.241144 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Apotik Kartini", "X": 107.00152, "Y": -6.248028, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.00152, -6.248028 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "RS Bhakti Kartini", "X": 107.00152, "Y": -6.248028, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.00152, -6.248028 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Shell Diponegoro Tambun-1 BKS", "X": 107.033371, "Y": -6.253852, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.033371, -6.253852 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "SPBU Pertamina Juanda Bekasi\r\n", "X": 107.027978, "Y": -6.251463, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.027978, -6.251463 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Gereja Pantekosta", "X": 107.011074, "Y": -6.251924, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.011074, -6.251924 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Masjid Al-Futuhat Al-Attasiyah", "X": 107.021243, "Y": -6.248532, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.021243, -6.248532 ] } }
+]
+}
